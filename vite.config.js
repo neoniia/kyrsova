afba84ce084916bcb09197c10a589bf3b8662403
neoniia <neoniia@users.noreply.github.com>
2026-02-02 13:24:07 +0100

    fix: update base path for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import { resolve } from 'path';
 const __dirname = fileURLToPath(new URL('.', import.meta.url));
 
 export default defineConfig({
-  base: '/',
+  base: '/kyrsova/',
   root: './src',
   publicDir: resolve(__dirname, 'public'),
   build: {
